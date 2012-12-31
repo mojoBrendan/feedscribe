0a84b0c2574c3da8d970fbdb2777e9e3b58dba3c
mojoBrendan <brendan@oddsoftware.net>
2012-12-31 11:49:54 +1000

    add gpl license and copyright headers

--- src/net/oddsoftware/android/feedscribe/data/ShortFeedItem.java
+++ src/net/oddsoftware/android/feedscribe/data/ShortFeedItem.java
@@ -1,3 +1,20 @@
+/*
+ *  Copyright 2012 Brendan McCarthy (brendan@oddsoftware.net)
+ *
+ *  This file is part of Feedscribe.
+ *
+ *  Feedscribe is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 3 
+ *  as published by the Free Software Foundation.
+ *
+ *  Feedscribe is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Feedscribe.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.oddsoftware.android.feedscribe.data;
 
 public class ShortFeedItem implements Comparable<ShortFeedItem>
